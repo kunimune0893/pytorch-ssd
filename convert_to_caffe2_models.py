98edcdc2d4bde420e8dbdbfbac213045a327cfb9
kunimune <kunimune.daisuke@gmail.com>
2019-12-07 08:17:56 +0000

    for onnx

--- convert_to_caffe2_models.py
+++ convert_to_caffe2_models.py
@@ -35,6 +35,7 @@ else:
     print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
     sys.exit(1)
 net.load(model_path)
+net = net.cuda()
 net.eval()
 
 model_path = f"models/{net_type}.onnx"
@@ -44,6 +45,7 @@ predict_net_path = f"models/{net_type}_predict_net.pb"
 predict_net_txt_path = f"models/{net_type}_predict_net.pbtxt"
 
 dummy_input = torch.randn(1, 3, 300, 300)
+dummy_input = dummy_input.cuda()
 torch.onnx.export(net, dummy_input, model_path, verbose=False, output_names=['scores', 'boxes'])
 
 model = onnx.load(model_path)
