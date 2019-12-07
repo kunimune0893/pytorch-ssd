08351abd98698e066d6e6a7f2369e8d8a3bd351f
kunimune <kunimune.daisuke@gmail.com>
2019-12-07 08:11:04 +0000

    supported: from file

--- eval_ssd.py
+++ eval_ssd.py
@@ -141,15 +141,19 @@ if __name__ == '__main__':
         net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
     elif args.net == 'mb2-ssd-lite':
         net = create_mobilenetv2_ssd_lite(len(class_names), width_mult=args.mb2_width_mult, is_test=True)
+    elif args.net == 'from-file':
+        net = None
     else:
         logging.fatal("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
         parser.print_help(sys.stderr)
         sys.exit(1)  
 
-    timer.start("Load Model")
-    net.load(args.trained_model)
-    net = net.to(DEVICE)
-    print(f'It took {timer.end("Load Model")} seconds to load the model.')
+    if net is not None:
+        timer.start("Load Model")
+        net.load(args.trained_model)
+        net = net.to(DEVICE)
+        print(f'It took {timer.end("Load Model")} seconds to load the model.')
+    
     if args.net == 'vgg16-ssd':
         predictor = create_vgg_ssd_predictor(net, nms_method=args.nms_method, device=DEVICE)
     elif args.net == 'mb1-ssd':
@@ -160,40 +164,44 @@ if __name__ == '__main__':
         predictor = create_squeezenet_ssd_lite_predictor(net,nms_method=args.nms_method, device=DEVICE)
     elif args.net == 'mb2-ssd-lite':
         predictor = create_mobilenetv2_ssd_lite_predictor(net, nms_method=args.nms_method, device=DEVICE)
+    elif args.net == 'from-file':
+        predictor = None
     else:
         logging.fatal("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    results = []
-    for i in range(len(dataset)):
-        print("process image", i)
-        timer.start("Load Image")
-        image = dataset.get_image(i)
-        print("Load Image: {:4f} seconds.".format(timer.end("Load Image")))
-        timer.start("Predict")
-        boxes, labels, probs = predictor.predict(image)
-        print("Prediction: {:4f} seconds.".format(timer.end("Predict")))
-        indexes = torch.ones(labels.size(0), 1, dtype=torch.float32) * i
-        results.append(torch.cat([
-            indexes.reshape(-1, 1),
-            labels.reshape(-1, 1).float(),
-            probs.reshape(-1, 1),
-            boxes + 1.0  # matlab's indexes start from 1
-        ], dim=1))
-    results = torch.cat(results)
-    for class_index, class_name in enumerate(class_names):
-        if class_index == 0: continue  # ignore background
-        prediction_path = eval_path / f"det_test_{class_name}.txt"
-        with open(prediction_path, "w") as f:
-            sub = results[results[:, 1] == class_index, :]
-            for i in range(sub.size(0)):
-                prob_box = sub[i, 2:].numpy()
-                image_id = dataset.ids[int(sub[i, 0])]
-                print(
-                    image_id + " " + " ".join([str(v) for v in prob_box]),
-                    file=f
-                )
+    if predictor is not None:
+        results = []
+        for i in range(len(dataset)):
+            print("process image", i)
+            timer.start("Load Image")
+            image = dataset.get_image(i)
+            print("Load Image: {:4f} seconds.".format(timer.end("Load Image")))
+            timer.start("Predict")
+            boxes, labels, probs = predictor.predict(image)
+            print("Prediction: {:4f} seconds.".format(timer.end("Predict")))
+            indexes = torch.ones(labels.size(0), 1, dtype=torch.float32) * i
+            results.append(torch.cat([
+                indexes.reshape(-1, 1),
+                labels.reshape(-1, 1).float(),
+                probs.reshape(-1, 1),
+                boxes + 1.0  # matlab's indexes start from 1
+            ], dim=1))
+        results = torch.cat(results)
+        for class_index, class_name in enumerate(class_names):
+            if class_index == 0: continue  # ignore background
+            prediction_path = eval_path / f"det_test_{class_name}.txt"
+            with open(prediction_path, "w") as f:
+                sub = results[results[:, 1] == class_index, :]
+                for i in range(sub.size(0)):
+                    prob_box = sub[i, 2:].numpy()
+                    image_id = dataset.ids[int(sub[i, 0])]
+                    print(
+                        image_id + " " + " ".join([str(v) for v in prob_box]),
+                        file=f
+                    )
+    
     aps = []
     print("\n\nAverage Precision Per-class:")
     for class_index, class_name in enumerate(class_names):
